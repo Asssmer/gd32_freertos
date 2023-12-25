b3cc14407597483b423fe791c9e8a4d735a13fc6
Asssmer <47934306+Asssmer@users.noreply.github.com>
2023-12-25 16:47:57 +0800

    motor_contro
    contro

---
+++ RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,99 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'gd32f450_freertos' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "gd32f4xx.h"
+
+/* ARM.FreeRTOS::RTOS:Config:CMSIS RTOS2:10.5.1 */
+#define RTE_RTOS_FreeRTOS_CONFIG_RTOS2  /* RTOS FreeRTOS Config for CMSIS RTOS2 API */
+/* ARM.FreeRTOS::RTOS:Core:Cortex-M:10.5.1 */
+#define RTE_RTOS_FreeRTOS_CORE          /* RTOS FreeRTOS Core */
+/* ARM.FreeRTOS::RTOS:Event Groups:10.5.1 */
+#define RTE_RTOS_FreeRTOS_EVENTGROUPS   /* RTOS FreeRTOS Event Groups */
+/* ARM.FreeRTOS::RTOS:Heap:Heap_4:10.5.1 */
+#define RTE_RTOS_FreeRTOS_HEAP_4        /* RTOS FreeRTOS Heap 4 */
+/* ARM.FreeRTOS::RTOS:Message Buffer:10.5.1 */
+#define RTE_RTOS_FreeRTOS_MESSAGE_BUFFER /* RTOS FreeRTOS Message Buffers */
+/* ARM.FreeRTOS::RTOS:Stream Buffer:10.5.1 */
+#define RTE_RTOS_FreeRTOS_STREAM_BUFFER /* RTOS FreeRTOS Stream Buffers */
+/* ARM.FreeRTOS::RTOS:Timers:10.5.1 */
+#define RTE_RTOS_FreeRTOS_TIMERS        /* RTOS FreeRTOS Timers */
+/* ARM::CMSIS:RTOS2:FreeRTOS:Cortex-M:10.5.1 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_FreeRTOS        /* CMSIS-RTOS2 FreeRTOS */
+/* ARM::CMSIS:RTOS:FreeRTOS:10.5.1 */
+#define RTE_CMSIS_RTOS                  /* CMSIS-RTOS */
+        #define RTE_CMSIS_RTOS_FreeRTOS         /* CMSIS-RTOS FreeRTOS */
+/* GigaDevice::Device:StdPeripherals:ADC:3.0.2 */
+#define RTE_DEVICE_STDPERIPHERALS_ADC
+/* GigaDevice::Device:StdPeripherals:CAN:3.0.2 */
+#define RTE_DEVICE_STDPERIPHERALS_CAN
+/* GigaDevice::Device:StdPeripherals:CRC:3.0.2 */
+#define RTE_DEVICE_STDPERIPHERALS_CRC
+/* GigaDevice::Device:StdPeripherals:CTC:3.0.2 */
+#define RTE_DEVICE_STDPERIPHERALS_CTC
+/* GigaDevice::Device:StdPeripherals:DAC:3.0.2 */
+#define RTE_DEVICE_STDPERIPHERALS_DAC
+/* GigaDevice::Device:StdPeripherals:DBG:3.0.2 */
+#define RTE_DEVICE_STDPERIPHERALS_DBG
+/* GigaDevice::Device:StdPeripherals:DCI:3.0.2 */
+#define RTE_DEVICE_STDPERIPHERALS_DCI
+/* GigaDevice::Device:StdPeripherals:DMA:3.0.2 */
+#define RTE_DEVICE_STDPERIPHERALS_DMA
+/* GigaDevice::Device:StdPeripherals:ENET:3.0.2 */
+#define RTE_DEVICE_STDPERIPHERALS_ENET
+/* GigaDevice::Device:StdPeripherals:EXMC:3.0.2 */
+#define RTE_DEVICE_STDPERIPHERALS_EXMC
+/* GigaDevice::Device:StdPeripherals:EXTI:3.0.2 */
+#define RTE_DEVICE_STDPERIPHERALS_EXTI
+/* GigaDevice::Device:StdPeripherals:FMC:3.0.2 */
+#define RTE_DEVICE_STDPERIPHERALS_FMC
+/* GigaDevice::Device:StdPeripherals:FWDGT:3.0.2 */
+#define RTE_DEVICE_STDPERIPHERALS_FWDGT
+/* GigaDevice::Device:StdPeripherals:GPIO:3.0.2 */
+#define RTE_DEVICE_STDPERIPHERALS_GPIO
+/* GigaDevice::Device:StdPeripherals:I2C:3.0.2 */
+#define RTE_DEVICE_STDPERIPHERALS_I2C
+/* GigaDevice::Device:StdPeripherals:IPA:3.0.2 */
+#define RTE_DEVICE_STDPERIPHERALS_IPA
+/* GigaDevice::Device:StdPeripherals:IREF:3.0.2 */
+#define RTE_DEVICE_STDPERIPHERALS_IREF
+/* GigaDevice::Device:StdPeripherals:MISC:3.0.2 */
+#define RTE_DEVICE_STDPERIPHERALS_MISC
+/* GigaDevice::Device:StdPeripherals:PMU:3.0.2 */
+#define RTE_DEVICE_STDPERIPHERALS_PMU
+/* GigaDevice::Device:StdPeripherals:RCU:3.0.2 */
+#define RTE_DEVICE_STDPERIPHERALS_RCU
+/* GigaDevice::Device:StdPeripherals:RTC:3.0.2 */
+#define RTE_DEVICE_STDPERIPHERALS_RTC
+/* GigaDevice::Device:StdPeripherals:SDIO:3.0.2 */
+#define RTE_DEVICE_STDPERIPHERALS_SDIO
+/* GigaDevice::Device:StdPeripherals:SPI_I2S:3.0.2 */
+#define RTE_DEVICE_STDPERIPHERALS_SPI
+/* GigaDevice::Device:StdPeripherals:SYSCFG:3.0.2 */
+#define RTE_DEVICE_STDPERIPHERALS_SYSCFG
+/* GigaDevice::Device:StdPeripherals:TIMER:3.0.2 */
+#define RTE_DEVICE_STDPERIPHERALS_TIMER
+/* GigaDevice::Device:StdPeripherals:TLI:3.0.2 */
+#define RTE_DEVICE_STDPERIPHERALS_TLI
+/* GigaDevice::Device:StdPeripherals:TRNG:3.0.2 */
+#define RTE_DEVICE_STDPERIPHERALS_TRNG
+/* GigaDevice::Device:StdPeripherals:USART:3.0.2 */
+#define RTE_DEVICE_STDPERIPHERALS_USART
+/* GigaDevice::Device:StdPeripherals:WWDGT:3.0.2 */
+#define RTE_DEVICE_STDPERIPHERALS_WWDGT
+
+
+#endif /* RTE_COMPONENTS_H */
--- main.c
+++ main.c
@@ -1,3 +1,111 @@
+#include "FreeRTOS.h"
+#include "task.h"
+#include "./454software/454software.h"
+
+// 任务函数原型
+void motor_speed_set(void *pvParameters);
+void sensor_get(void *pvParameters);
+
+// 电机
+extern volatile MotorStatus motor_status;
+// ADC
+extern volatile uint16_t adc_values_454[ADC_CHANNEL_COUNT];
+extern volatile SensorData sensor_data;
+extern char usart0_res[12];
+// 温度
+float temperature_P7;
+float temperature_P9;
+// 压力
+float pressure_P10;
+float pressure_P11;
+float pressure_P12;
+// 流量
+float flow_P13;
+float flow_P14;
+float flow_P15;
+
 int main()
 {
+    init_454();
+    P4_PWM_set(0);
+    P5_PWM_set(0);
+    P6_PWM_set(0);
+    motor_control(300);
+
+    xTaskCreate(sensor_get, "sensor_get", 1024, (void *)1, 15, NULL);
+    xTaskCreate(motor_speed_set, "speed_set", 1024, (void *)1, 15, NULL);
+    // 启动调度器
+    vTaskStartScheduler();
+    for (;;)
+        ;
+    return 0;
+}
+
+// 任务实现
+void motor_speed_set(void *pvParameters)
+{
+    // int taskNumber = (int)pvParameters;
+    // uint16_t motor_control_data = 0;
+    // motor_control(0);
+    // while (1)
+    // {
+    //     gpio_bit_toggle(GPIOG, GPIO_PIN_7);
+    //     delay_ms_454(1000);
+    // }
+    // for (;;)
+    // {
+    //     if (scanf("%hu", &motor_control_data) == 1)
+    //     {
+    //         motor_control(motor_control_data);
+    //         printf("Set motor speed:%hu\n", motor_control_data);
+    //         motor_control_data = 0;
+    //     }
+    //     else
+    //     {
+    //         motor_control_data = 0;
+    //     }
+    // }
+}
+
+void sensor_get(void *pvParameters)
+{
+    int taskNumber = (int)pvParameters;
+    for (;;)
+    {
+        temperature_P7 = P7_get();
+        temperature_P9 = P9_get();
+        pressure_P10 = P10_get();
+        pressure_P11 = P11_get();
+        pressure_P12 = P12_get();
+        flow_P13 = P13_get();
+        flow_P14 = P14_get();
+        flow_P15 = P15_get();
+        // gpio_bit_toggle(GPIOG, GPIO_PIN_6);
+        // delay_ms_454(1000);
+
+        printf("\nmotor_speed:%.2f\n", motor_status.current_speed);
+        printf("\nmotor_temp:%.2f\n", motor_status.motor_temperature);
+        printf("\ntemperature_P7:%.2f\n", temperature_P7);
+        printf("\ntemperature_P9:%.2f\n", temperature_P9);
+        printf("\npressure_P10:%.2f\n", pressure_P10);
+        printf("\npressure_P11:%.2f\n", pressure_P11);
+        printf("\npressure_P12:%.2f\n", pressure_P12);
+        printf("\nflow_P13:%.2f\n", flow_P13);
+        printf("\nflow_P14:%.2f\n", flow_P14);
+        printf("\nflow_P15:%.2f\n", flow_P15);
+        printf("\n-------------------------------");
+    }
+}
+
+//-----------------------------------------------------------------------
+//                       HOOK
+//-----------------------------------------------------------------------
+void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName)
+{
+    // 打印发生堆栈溢出的任务名称
+    printf("\nStack overflow in task: ");
+    printf(pcTaskName);
+    // 进入一个无限循环，停止任务执行
+    for (;;)
+        ;
 }
